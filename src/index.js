3216d05e7a7c7c3d73f7d3fa4c7c30577eaa5d09
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-24 12:15:03 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+ document.querySelector('form').addEventListener('submit',(e)=>{
+  e.preventDefault()
+  handleToDo(e.target.new-task-description.value)
+ })
+})
+
+function handleToDo(todo){
+  let li = document.createElement('li')
+  li.textContent = todo
+  document.querySelector("#tasks").appendChild(li)
+}
