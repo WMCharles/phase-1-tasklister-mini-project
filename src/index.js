38399dc6c277ca56ea474c2a5bebe5bac3768185
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-24 15:30:09 +0300

    clear input field after submitting form

--- src/index.js
+++ src/index.js
@@ -10,4 +10,5 @@ function handleTask(todo){
   let li = document.createElement('li')
   li.innerText = todo
   document.getElementById('tasks').appendChild(li)
+  document.getElementById('new_task_description').value = "";
 }
